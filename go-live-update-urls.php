500d05cd7b148ee5789f46a952768d37407102d5
Mat Lipe <Mat Lipe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-26 22:31:12 +0000

    tagging the new version throughout
    git-svn-id: https://plugins.svn.wordpress.org/go-live-update-urls/trunk@537004 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- go-live-update-urls.php
+++ go-live-update-urls.php
@@ -5,7 +5,7 @@ Plugin URI: http://lipeimagination.info/
 Description: This Plugin Updates all the URLs in the database to point to the new URL when making your site live or channging domains.
 Author: Mat Lipe
 Author URI: http://lipeimagination/
-Version: 1.2
+Version: 1.2.1
 */
 /*  Copyright 2011 Mat Lipe (mat@lipeimagination.info);
 
