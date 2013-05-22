6ce44605953bb2712839c8c66ca639db98af4242
Mat Lipe <Mat Lipe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-22 13:27:07 +0000

    Bringing in version 1.8.0 which adds a couple new filters for more direct link modifications
    git-svn-id: https://plugins.svn.wordpress.org/simple-links/tags/1.8.0@716642 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ simple-links.php
@@ -0,0 +1,23 @@
+<?php 
+/*
+Plugin Name: Simple Links
+Plugin URI: http://lipeimagination.info/simple-links-docs/
+Description: Replacement for Wordpress Links Manager with many added features.
+Version: 1.8.0
+Author: Mat Lipe
+Author URI: http://lipeimagination.info
+*/
+
+
+define( 'SIMPLE_LINKS_IMG_DIR', plugin_dir_url(__FILE__). 'img/' );
+define( 'SIMPLE_LINKS_JS_DIR',  plugin_dir_url(__FILE__). 'js/' );
+define( 'SIMPLE_LINKS_JS_PATH',  plugin_dir_path(__FILE__). 'js/' );
+define( 'SIMPLE_LINKS_CSS_DIR', plugin_dir_url(__FILE__). 'css/' );
+define( 'SIMPLE_LINKS_SHORTCODE_DIR', plugin_dir_path(__FILE__). 'shortcode/' );
+
+require( 'simple-links-functions.php' );
+require( 'classes/init.php' );
+require( 'widgets/init.php');
+
+
+
