2cf129f515015f3451cb2ab58676078c24ea1848
Mat Lipe <Mat Lipe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-23 21:17:12 +0000

    Bringing in version 4.1.4 which is a little more forgiving of other plugins
    git-svn-id: https://plugins.svn.wordpress.org/advanced-sidebar-menu/trunk@717485 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advanced-sidebar-menu.php
+++ advanced-sidebar-menu.php
@@ -4,24 +4,25 @@ Plugin Name: Advanced Sidebar Menu
 Plugin URI: http://lipeimagination.info/wordpress/advanced-sidebar-menu/
 Description: Creates dynamic menu based on child/parent relationship.
 Author: Mat Lipe
-Version: 4.1.3
+Version: 4.1.4
 Author URI: http://lipeimagination.info
-Since: 4.23.13
+Since: 5.23.13
 */
 
-#-- Bring in the functions
-require( 'lib/advancedSidebarMenu.php' );
-$asm = new advancedSidebarMenu();
+
+#-- Define Constants
+define( 'ADVANCED_SIDEBAR_DIR', plugin_dir_path(__FILE__) );
+define( 'ADVANCED_SIDEBAR_WIDGETS_DIR', ADVANCED_SIDEBAR_DIR . 'widgets/' );
+define( 'ADVANCED_SIDEBAR_VIEWS_DIR', ADVANCED_SIDEBAR_DIR . 'views/' );
+define( 'ADVANCED_SIDEBAR_LEGACY_DIR', ADVANCED_SIDEBAR_DIR . 'legacy/' );
 
 
 #-- Bring in the Widgets
-require( 'widgets/init.php' );
+require( ADVANCED_SIDEBAR_WIDGETS_DIR.'init.php' );
+#-- Bring in the functions
+require( ADVANCED_SIDEBAR_DIR.'lib/advancedSidebarMenu.php' );
+$asm = new advancedSidebarMenu();
 
-#-- Define Constants
-define( 'ADVANCED_SIDEBAR_WIDGETS_DIR', plugin_dir_path(__FILE__) . 'widgets/' );
-define( 'ADVANCED_SIDEBAR_VIEWS_DIR', plugin_dir_path(__FILE__) . 'views/' );
-define( 'ADVANCED_SIDEBAR_LEGACY_DIR', plugin_dir_path(__FILE__) . 'legacy/' );
-define( 'ADVANCED_SIDEBAR_DIR', plugin_dir_path(__FILE__) );
 
 
 #-- Bring in the JQuery
