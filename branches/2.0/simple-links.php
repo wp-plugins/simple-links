e0040107ea34b6746cdfb7d6cb4be2504c2f9c68
Mat Lipe <Mat Lipe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-16 13:29:56 +0000

    Updated existing files structure to prepare for new improved organizational technique
    git-svn-id: https://plugins.svn.wordpress.org/simple-links@773333 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- branches/2.0/simple-links.php
+++ branches/2.0/simple-links.php
@@ -1,23 +1,37 @@
 <?php 
 /*
 Plugin Name: Simple Links
-Plugin URI: http://lipeimagination.info/simple-links-docs/
+Plugin URI: http://matlipe.com/simple-links-docs/
 Description: Replacement for Wordpress Links Manager with many added features.
-Version: 1.9.2
+Version: 2.0
 Author: Mat Lipe
-Author URI: http://lipeimagination.info/mat
+Author URI: http://matlipe.com/
 */
 
+define( 'SIMPLE_LINKS_DIR', plugin_dir_path(__FILE__) );
+define( 'SIMPLE_LINKS_URL', plugin_dir_url(__FILE__) );
+define( 'SIMPLE_LINKS_ASSETS_URL', SIMPLE_LINKS_URL. 'assets/');
+define( 'SIMPLE_LINKS_IMG_DIR', SIMPLE_LINKS_ASSETS_URL. 'img/' );
+define( 'SIMPLE_LINKS_JS_DIR',  SIMPLE_LINKS_ASSETS_URL. 'js/' );
+define( 'SIMPLE_LINKS_JS_PATH',  SIMPLE_LINKS_DIR. 'assets/js/' );
+define( 'SIMPLE_LINKS_CSS_DIR', SIMPLE_LINKS_ASSETS_URL. 'css/' );
+define( 'SIMPLE_LINKS_SHORTCODE_DIR', SIMPLE_LINKS_DIR. 'shortcode/' );
 
-define( 'SIMPLE_LINKS_IMG_DIR', plugin_dir_url(__FILE__). 'img/' );
-define( 'SIMPLE_LINKS_JS_DIR',  plugin_dir_url(__FILE__). 'js/' );
-define( 'SIMPLE_LINKS_JS_PATH',  plugin_dir_path(__FILE__). 'js/' );
-define( 'SIMPLE_LINKS_CSS_DIR', plugin_dir_url(__FILE__). 'css/' );
-define( 'SIMPLE_LINKS_SHORTCODE_DIR', plugin_dir_path(__FILE__). 'shortcode/' );
-
-require( 'simple-links-functions.php' );
-require( 'classes/init.php' );
+require( 'includes/functions.php' );
 require( 'widgets/init.php');
+
+
+require('includes/SL_post_type_tax.php');
+require('includes/simple_links.php');
+
+
+$simple_links = new simple_links();
+
+
+if( is_admin() ){
+    require( 'includes/simple_links_admin.php' );
+    $simple_links_admin_func = new simple_links_admin();
+}
 
 
 
