6474b8b5230aff2c27a13c58f226cef9c3d2d009
Mat Lipe <Mat Lipe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-18 02:10:29 +0000

    Ready to start building onto the SimpleLinksTheLink
    git-svn-id: https://plugins.svn.wordpress.org/simple-links/branches/2.0@774360 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/SimpleLinksFactory.php
+++ includes/SimpleLinksFactory.php
@@ -9,30 +9,29 @@
  * @uses May be constructed with $args then $this->output() will output the links list
  */
 class SimpleLinksFactory{
-    public $args = array(); //the list args
-    
+    public $full_args = array(); //combined args and query args
     public $links = array(); //the retrieved links
     public $type = false; //if this is a shortcode or widget etc.
     
-    //Default args
-    public $defaults = array(  
-                       'title'              => false,
-                       'show_image'         => false,
-                       'show_image_only'    => false,
-                       'image_size'         => 'thumbnail',
-                       'fields'             => false,
-                       'description'        => false,
-                       'separator'          =>  '-',
-                       'id'                 => false,
-                       'remove_line_break'  => false
+    //Default args - used for output
+    public $args = array(  
+       'title'              => false,
+       'show_image'         => false,
+       'show_image_only'    => false,
+       'image_size'         => 'thumbnail',
+       'fields'             => false,
+       'description'        => false,
+       'separator'          =>  '-',
+       'id'                 => false,
+       'remove_line_break'  => false
       );
     
       //Default Query Args - used by getLinks();
       public $query_args = array(
-            'order'    => 'ASC',
-            'orderby'  => 'menu_order',
-            'count'    => '-1',
-            'category' => false,
+         'order'    => 'ASC',
+         'orderby'  => 'menu_order',
+         'count'    => '-1',
+         'category' => false,
       );  
     
     
@@ -74,7 +73,7 @@ class SimpleLinksFactory{
      * @uses echo $links
      */
     function __toString(){
-        return $this->output();
+        return $this->output(false);
     }
     
     
@@ -101,6 +100,8 @@ class SimpleLinksFactory{
         //Merge with defaults - done this way to split to two lists
         $this->args = shortcode_atts($this->args, $args); 
         $this->query_args = shortcode_atts($this->query_args, $args);
+        
+        $this->full_args = array_merge( $this->args, $this->query_args);
            
          
         //Change the Random att to rand for get posts
@@ -160,16 +161,14 @@ class SimpleLinksFactory{
         
         //Get the links
         $links = get_posts( $this->query_args );
-        
-        $full_args = array_merge($this->args, $this->query_args);
-        
+
         $links = apply_filters( 'simple_links_object', $links, $full_args );
 
 
         //backwards compatible
         if( $this->type == 'shortcode' ){
-            $links = apply_filters('simple_links_shortcode_links_object', $links, $full_args);
-            $links = apply_filters('simple_links_shortcode_links_object_' . $this->args['id'], $links, $full_args );      
+            $links = apply_filters('simple_links_shortcode_links_object', $links, $this->full_args);
+            $links = apply_filters('simple_links_shortcode_links_object_' . $this->args['id'], $links, $this->full_args );      
         }
   
         return $this->links = $links;
@@ -179,10 +178,50 @@ class SimpleLinksFactory{
     /**
      * Generated the output bases on retrieved links
      * 
-     * @return String
+     *
      * @uses may be called normally or by using echo with the class
+     * @uses SimpleLinksTheLink
+     * 
+     * @param bool $echo - defaults to true
+     * @return String|void
      */
-    private function output(){
+    private function output($echo = true){
+        
+        if( empty( $this->links ) ) return false;
+        
+        
+        //if there is a title
+        if( $this->args['title'] ){
+            $output .= sprintf('<h4 class="simple-links-title">%s</h4>', $this->args['title'] );
+            
+        }
+        
+        //Start the list
+        if( $this->args['id'] ){
+            $output .= '<ul class="simple-links-list" id="' . $this->args['id'] . '">';
+        } else {
+            $output .= '<ul class="simple-links-list">';
+        }
+            
+            //Add the links to the list
+            foreach( $links as $link ){
+                
+                $link = new SimpleLinksTheLink($link, $this->args);
+                
+                $output .= $link->output();   
+            }
+ 
+        //end the list
+        $output .= '</ul><!-- End .simple-links-list -->';
+        
+        
+        
+        // Backwards compatibility
+        if( $this->type == 'shortcode' ){
+            $output =  apply_filters( 'simple_links_shortcode_output', $output, $links, $this->full_args );
+            $output = apply_filters( 'simple_links_shortcode_output_' . $this->args['id'], $output, $links, $this->full_args );
+        }
+        
         
         
     }
