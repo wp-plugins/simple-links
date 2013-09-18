7901a3cc30df6185c87c1a77efd0b97fb5035e55
Mat Lipe <Mat Lipe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-18 01:35:21 +0000

    Making some more progress on the SimpleLinksFactory class - still working on shortcode transition
    git-svn-id: https://plugins.svn.wordpress.org/simple-links/branches/2.0@774339 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/SimpleLinksFactory.php
+++ includes/SimpleLinksFactory.php
@@ -10,26 +10,30 @@
  */
 class SimpleLinksFactory{
     public $args = array(); //the list args
-    public $query_args = array(); //the args used by getLinks()
+    
     public $links = array(); //the retrieved links
     public $type = false; //if this is a shortcode or widget etc.
     
     //Default args
     public $defaults = array(  
                        'title'              => false,
-                       'category'           => false,
-                       'orderby'            => 'menu_order',
-                       'count'              => '-1',
                        'show_image'         => false,
                        'show_image_only'    => false,
                        'image_size'         => 'thumbnail',
-                       'order'              => 'ASC',
                        'fields'             => false,
                        'description'        => false,
                        'separator'          =>  '-',
                        'id'                 => false,
                        'remove_line_break'  => false
       );
+    
+      //Default Query Args - used by getLinks();
+      public $query_args = array(
+            'order'    => 'ASC',
+            'orderby'  => 'menu_order',
+            'count'    => '-1',
+            'category' => false,
+      );  
     
     
     
@@ -88,19 +92,40 @@ class SimpleLinksFactory{
             }
          }
         
-        
-        $args = wp_parse_args($args, $this->defaults);
+        //Merge with defaults - done this way to split to two lists
+        $this->args = shortcode_atts($this->args, $args); 
+        $this->query_args = shortcode_atts($this->query_args, $args);
+           
          
         //Change the Random att to rand for get posts
-        if( $args['orderby'] == 'random' ){
-            $args['orderby'] = 'rand';
+        if( $this->query_args['orderby'] == 'random' ){
+            $this->query_args['orderby'] = 'rand';
         }
         
         
         //Setup the fields
-        if( $args['fields'] != false ){
-            $args['fields'] = explode(',', $args['fields'] );
+        if( $this->args['fields'] != false ){
+            $this->args['fields'] = explode(',', $this->args['fields'] );
         }
+        
+        
+        //Add the categories to the query
+        if( $this->query_args['category'] ){
+            $args_cats = explode(',', $this->query_args['category']);
+            //Go through all the possible categories and add the ones that are set
+            foreach( $this->get_categories() as $cat ){
+                if( in_array($cat, $args_cats) ){
+                    $cat = get_term_by('name', $cat, 'simple_link_category');
+                    $all_cats[] = $cat->term_id;
+                }
+            }
+            $this->query_args['tax_query'][] = array(
+                        'taxonomy' => 'simple_link_category',
+                        'fields'   => 'id',
+                        'terms'    =>  $all_cats
+            );
+        }
+        
 
     }
     
@@ -109,9 +134,17 @@ class SimpleLinksFactory{
      * Retrieve the proper links based on argument set earlier
      * 
      * @return obj
+     * 
+     * @since 9.17.13
      */
     private function getLinks(){
         
+        
+        $this->query_args['post_type'] = 'simple_link';
+        $this->query_args['posts_per_page'] = $this->query_args['count'];
+        $this->query_args['posts_per_archive_page'] = $this->query_args['count'];
+        
+  
         
     }
     
